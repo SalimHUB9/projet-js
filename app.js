5ae0710205d97dd2d7de7d2646f3c9d835aa844e
Salim Bedraoui <salim.bedraoui@gmail.com>
2025-12-30 21:04:19 +0100

    modification dans l'app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-document.querySelectorAll(".sidebar a").forEach(link=>{
-  link.onclick = ()=>{
-    document.querySelectorAll(".sidebar a").forEach(a=>a.classList.remove("active"));
+document.querySelectorAll(".sidebar a").forEach(link => {
+  link.onclick = () => {
+
+    document.querySelectorAll(".sidebar a").forEach(a => a.classList.remove("active"));
     link.classList.add("active");
 
-    document.querySelectorAll(".section").forEach(s=>s.classList.remove("active"));
-    document.getElementById(link.dataset.section).classList.add("active");
-  }
+    document.querySelectorAll(".section").forEach(s => s.classList.remove("active"));
+    const section = document.getElementById(link.dataset.section);
+    section.classList.add("active");
+
+    if (link.dataset.section === "api") {
+      initAPI();
+    }
+  };
 });
